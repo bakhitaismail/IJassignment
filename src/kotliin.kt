904caee7bd05d09b4f826e94639622594b87598e
bakhitaismail <bakhitaismailk@gmail.com>
2022-02-23 20:46:34 +0300

    second commit

---
+++ src/kotliin.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var userName: String = "Bakhita"
+    var age: Int = 20
+    printFullName()
+    sayHelloWorld()
+    favoriteFootballer()
+    println("My name is $userName and I am $age years old.")
+
+}
+
+fun printFullName() {
+    println("Bakhita Ismail")
+
+}
+fun sayHelloWorld() {
+    println("Hello, world.")
+}
+fun favoriteFootballer() {
+    println("Marcus Rashford.")
+}
